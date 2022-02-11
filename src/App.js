c3e5d0b1b44b4818c2d548f55746f1d223683934
borman73 <zk-borman@yandex.ru>
2022-02-11 21:01:32 +0400

    Pages and routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Switch, Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+
+
 function App() {
   return (
-    <div >
-      Hi
-    </div>
+    <Switch>
+      <Route path='/' exact>   
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+    </Switch>
   );
 }
 
