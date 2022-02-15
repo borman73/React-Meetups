3ef5a929162d42ca0f8d0a258175d13761a276a1
borman73 <zk-borman@yandex.ru>
2022-02-16 01:04:27 +0400

    Database requests added

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,70 @@
+import { useState, useEffect } from "react";
+
 import MeetupList from "../components/meetups/MeetupList";
 
-const MEETUPS_DATA = [
-  {
-    id: 'm1', 
-    title: 'First meetup',
-    image: 
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 1, 12345 Meetup City',
-    description: 'Bla-bla-bla 1',
-  },
-  {
-    id: 'm2', 
-    title: 'Second meetup',
-    image:
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 2, 12345 Meetup City',
-    description: 'Bla-bla-bla 2',
-  }
-];
+// const MEETUPS_DATA = [
+//   {
+//     id: 'm1', 
+//     title: 'First meetup',
+//     image: 
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 1, 12345 Meetup City',
+//     description: 'Bla-bla-bla 1',
+//   },
+//   {
+//     id: 'm2', 
+//     title: 'Second meetup',
+//     image:
+//     'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+//     address: 'Meetupstreet 2, 12345 Meetup City',
+//     description: 'Bla-bla-bla 2',
+//   }
+// ];
 
 function AllMeetupsPage() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      'database_url/meetups.json',
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: 'key',
+            ...data[key]
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+  
+  if (isLoading) {
     return (
-      <div>
-        <h1>All Meetups</h1>
-        <MeetupList meetup={MEETUPS_DATA}/>
-      </div>
+      <section>
+        Loading ...
+      </section>
     );
   }
+
+  return (
+    <div>
+      <h1>All Meetups</h1>
+      <MeetupList meetup={loadedMeetups} />
+    </div>
+  );
+}
   
-  export default AllMeetupsPage;
+export default AllMeetupsPage;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useHistory } from 'react-router';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage () {
-    return <section>
-      <h1>Add New Meetup</h1>
-      <NewMeetupForm />
-    </section>
+
+  const history = useHistory();
+
+  const addMeetupHandler = (meetupData) => {
+    fetch('database_url/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }
+    ).then(() => {
+        history.replace('/');
+    });
   }
+
+  return <div>
+    <h1>Add New Meetup</h1>
+    <NewMeetupForm onAddMeetup={addMeetupHandler} />
+  </div>
+}
   
-  export default NewMeetupPage;
+export default NewMeetupPage;
